8ce4d177f634a364e16b7b7ddb69987734339ea0
Renat Gubaev <renat.gubaev@gmail.com>
2022-12-06 16:09:41 +0300

    no message

--- platform/api/coin/ICoinExchangeDto.ts
+++ platform/api/coin/ICoinExchangeDto.ts
@@ -1,12 +1,17 @@
 
+import { CoinBalance } from '../../coin/CoinBalance';
 import { ITraceable } from '@ts-core/common';
+import { LedgerCoinId } from '../../../ledger/coin/LedgerCoinId';
 
 export interface ICoinExchangeGetDtoResponse extends ITraceable {
-    to: string;
-    from: string;
+    to: CoinBalance;
+    from: CoinBalance;
     rate: string;
 }
 
-export interface ICoinExchangeDto extends ICoinExchangeGetDtoResponse {
+export interface ICoinExchangeDto extends ITraceable {
+    to: LedgerCoinId;
+    from: LedgerCoinId;
+    rate: string;
     amount: string;
 }
