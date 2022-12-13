e9ff9f6ae27de086525e71318350d19cb2357259
Renat Gubaev <renat.gubaev@gmail.com>
2022-12-13 19:01:06 +0300

    no message

--- platform/voting/VotingVote.ts
+++ platform/voting/VotingVote.ts
@@ -1,11 +1,14 @@
 import { Type } from 'class-transformer';
 import { IsEnum, IsString } from 'class-validator';
 import { LedgerVoteType } from "../../ledger/voting";
+import { User } from '../user';
 
 export class VotingVote {
     id: number;
     userId: number;
     stepId: number;
+
+    user?: User;
 
     @IsEnum(LedgerVoteType)
     type: LedgerVoteType;
