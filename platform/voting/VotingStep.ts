8ef220f2759dc26b4bde8509004d881b402e8528
Renat Gubaev <renat.gubaev@gmail.com>
2022-12-18 14:07:07 +0300

    no message

--- platform/voting/VotingStep.ts
+++ platform/voting/VotingStep.ts
@@ -1,10 +1,11 @@
 
 import { Type } from 'class-transformer';
 import { LedgerRoles } from '../../ledger/role';
+import { ILedgerVotingRestriction } from '../../ledger/voting';
 import { ILedgerVotingState } from '../../ledger/voting/LedgerVotingState';
 import { LedgerVotingStepType } from '../../ledger/voting/LedgerVotingStepType';
 
-export class VotingStep implements ILedgerVotingState {
+export class VotingStep implements ILedgerVotingState, ILedgerVotingRestriction {
     id: number;
     type: LedgerVotingStepType;
     index: number;
@@ -18,6 +19,10 @@ export class VotingStep implements ILedgerVotingState {
     votesResultPercent: number;
     votesAgainst: string;
     votesAgainstPercent: number;
+
+    votesForMinPercent?: number;
+    votesTotalMinPercent?: number;
+    votesAgainstMaxPercent?: number;
 
     total: string;
 
