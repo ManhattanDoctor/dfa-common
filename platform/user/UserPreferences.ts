397b78a666c04fb6c89d0c3b1bba891a9ebede5e
Renat Gubaev <gubaev.r@n-t.io>
2025-02-13 02:13:53 +0300

    no message

--- platform/user/UserPreferences.ts
+++ platform/user/UserPreferences.ts
@@ -1,8 +1,8 @@
 import * as _ from 'lodash';
 
 export class UserPreferences {
+    name: string;
     email: string;
-    name?: string;
     theme?: UserPreferencesTheme;
     phone?: string;
     picture?: string;
