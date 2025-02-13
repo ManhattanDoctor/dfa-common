ecd0102b4575f4fceb48d7ba43e5a8708720fb3b
Renat Gubaev <gubaev.r@n-t.io>
2025-02-13 19:25:24 +0300

    no message

---
+++ platform/util/ImageUtil.ts
@@ -0,0 +1,21 @@
+import { Sha512 } from "@ts-core/common";
+import * as _ from "lodash";
+
+export class ImageUtil {
+
+    // --------------------------------------------------------------------------
+    //
+    //  Static Methods
+    //
+    // --------------------------------------------------------------------------
+
+    public static getUser(uid: string): string {
+        uid = Sha512.hex(uid).substring(0, 32);
+        return `https://www.gravatar.com/avatar/${Sha512.hex(uid)}?s=200&d=identicon&r=g`;
+    }
+
+    public static getCompany(uid: string): string {
+        uid = Sha512.hex(uid).substring(0, 32);
+        return `https://www.gravatar.com/avatar/${Sha512.hex(uid)}?s=200&d=retro&r=g`;
+    }
+}
