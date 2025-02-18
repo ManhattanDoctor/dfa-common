2a42561ea6199b2197619c4ac71631886d3fd55f
Renat Gubaev <gubaev.r@n-t.io>
2025-02-18 15:27:28 +0300

    no message

--- platform/Permission.ts
+++ platform/Permission.ts
@@ -1,36 +1,20 @@
-import { IOpenIdResourceValidationOptions, OpenIdResourceValidationOptions } from "@ts-core/openid-common";
+import { IOpenIdResourceValidationOptions } from "@ts-core/openid-common";
 
-export enum Resource {
-    USER = 'user',
-    COMPANY = 'company',
-}
-export enum ResourceScope {
-    ADD = 'add',
-    READ = 'read',
-    EDIT = 'edit',
-    REMOVE = 'remove',
+export enum ResourcePermission {
+    USER_ADD = 'user:add',
+    USER_READ = 'user:read',
+    USER_EDIT = 'user:edit',
+    USER_LIST = 'user:list',
     //
-    LIST = 'list',
-    REJECT = 'reject',
-    APPROVE = 'approve',
+    COMPANY_ADD = 'company:add',
+    COMPANY_READ = 'company:read',
+    COMPANY_EDIT = 'company:edit',
+    COMPANY_LIST = 'company:list',
+    COMPANY_REJECT = 'company:reject',
+    COMPANY_APPROVE = 'company:approve',
 }
 
-export const ResourcePermission = {
-    USER_ADD: `${Resource.USER}:${ResourceScope.ADD}`,
-    USER_READ: `${Resource.USER}:${ResourceScope.READ}`,
-    USER_EDIT: `${Resource.USER}:${ResourceScope.EDIT}`,
-    USER_LIST: `${Resource.USER}:${ResourceScope.LIST}`,
-    //
-    COMPANY_ADD: `${Resource.COMPANY}:${ResourceScope.ADD}`,
-    COMPANY_READ: `${Resource.COMPANY}:${ResourceScope.READ}`,
-    COMPANY_EDIT: `${Resource.COMPANY}:${ResourceScope.EDIT}`,
-    COMPANY_LIST: `${Resource.COMPANY}:${ResourceScope.LIST}`,
-    COMPANY_REJECT: `${Resource.COMPANY}:${ResourceScope.REJECT}`,
-    COMPANY_APPROVE: `${Resource.COMPANY}:${ResourceScope.APPROVE}`,
-}
-
-export type Permission = typeof ResourcePermission[keyof typeof ResourcePermission];
-export function getResourceValidationOptions(item: Permission): IOpenIdResourceValidationOptions {
+export function getResourceValidationOptions(item: ResourcePermission): IOpenIdResourceValidationOptions {
     let index = item.split(':');
     return { name: index[0], scope: index.slice(1) }
 }
