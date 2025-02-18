2f798bd8706be148a67c23d2d4e4d06a6dc47349
Renat Gubaev <gubaev.r@n-t.io>
2025-02-18 15:22:46 +0300

    no message

--- platform/Permission.ts
+++ platform/Permission.ts
@@ -9,16 +9,24 @@ export enum ResourceScope {
     READ = 'read',
     EDIT = 'edit',
     REMOVE = 'remove',
+    //
+    LIST = 'list',
+    REJECT = 'reject',
+    APPROVE = 'approve',
 }
 
 export const ResourcePermission = {
     USER_ADD: `${Resource.USER}:${ResourceScope.ADD}`,
     USER_READ: `${Resource.USER}:${ResourceScope.READ}`,
     USER_EDIT: `${Resource.USER}:${ResourceScope.EDIT}`,
+    USER_LIST: `${Resource.USER}:${ResourceScope.LIST}`,
     //
     COMPANY_ADD: `${Resource.COMPANY}:${ResourceScope.ADD}`,
     COMPANY_READ: `${Resource.COMPANY}:${ResourceScope.READ}`,
     COMPANY_EDIT: `${Resource.COMPANY}:${ResourceScope.EDIT}`,
+    COMPANY_LIST: `${Resource.COMPANY}:${ResourceScope.LIST}`,
+    COMPANY_REJECT: `${Resource.COMPANY}:${ResourceScope.REJECT}`,
+    COMPANY_APPROVE: `${Resource.COMPANY}:${ResourceScope.APPROVE}`,
 }
 
 export type Permission = typeof ResourcePermission[keyof typeof ResourcePermission];
