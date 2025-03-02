49390653686680c96b66b0eb6a86961d84af25c5
Renat Gubaev <gubaev.r@n-t.io>
2025-03-02 17:44:46 +0300

    no message

--- platform/api/entity/IEntity.ts
+++ platform/api/entity/IEntity.ts
@@ -9,23 +9,3 @@ export interface IEntity {
     picture: string;
     description?: string;
 }
-
-export function entityPicture(uid: string, options: IEntityPicture): string {
-    let { display, rating, size } = options;
-    if (_.isNil(display)) {
-        display = 'identicon';
-    }
-    if (_.isNil(rating)) {
-        rating = 'g';
-    }
-    if (_.isNil(size)) {
-        size = '200';
-    }
-    return `https://www.gravatar.com/avatar/${Sha512.hex(uid)}?s=${size}&d=${display}&r=${rating}`;
-}
-
-export interface IEntityPicture {
-    size?: string;
-    rating?: string;
-    display?: string;
-}
